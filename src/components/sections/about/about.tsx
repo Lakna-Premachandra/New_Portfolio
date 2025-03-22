3f91f6f5ea2c78cf9c3b3ecb7a4624efab160ddc
lakna <nilanilakna@gmail.com>
2025-03-22 21:48:17 +0530

    Minor Change

--- src/components/sections/about/about.tsx
+++ src/components/sections/about/about.tsx
@@ -1,10 +1,11 @@
 import { Button } from "@/components/ui";
+import logo from '../../../../public/images/google-icon-logo.svg'
 
 export const About = () => {
   return (
     <div className="text-md md:flex-row flex flex-col gap-4 p-6  items-center">
       <div>
-        <img className="rounded-sm " src="public/images/google-icon-logo.svg" height={300} width={300} alt="" />
+        <img className="rounded-sm " src={logo} height={300} width={300} alt="" />
       </div>
       <div className="flex flex-col text-right gap-4">
         <div className="text-sm md:text-right text-center">
